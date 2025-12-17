41b54bbdd6a257d7712c589ce71f6ba1fdae662c
kazedevs <businessrajkumardas@gmail.com>
2025-12-17 17:46:32 +0530

    implemented rate limiting

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import  express from "express";
 import dotenv from "dotenv";
 import routes from "./routes/routes.js"
 import connectDB from "../server/config/db.js"
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
@@ -9,12 +10,14 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use(express.json());
+app.use(rateLimiter);
 
 app.use("/api/notes", routes);
-connectDB();
 
 
-
-app.listen(5000, () => {
+connectDB().then(() => {
+    app.listen(5000, () => {
     console.log(`server started on http://localhost:${port}`)
-})
+    });
+});
+
