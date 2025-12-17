a37a10f5438c3e76481000052ea856ad0dcd2817
kazedevs <businessrajkumardas@gmail.com>
2025-12-17 14:02:35 +0530

    routes, model, config setup

---
+++ server/controllers/notesController.js
@@ -0,0 +1,25 @@
+
+
+export function getAllNotes(req, res){
+    res.status(200).send("all notes");
+};
+
+
+export function createNote(req, res){
+    res.status(201).send("Note created successfully");
+};
+
+export function updateNote(req, res){
+    res.status(201).json({
+        message: "Note updated successfully"
+    })
+};
+
+export function deleteNote(req, res){
+    res.status(201).json({
+        message: "Note deleted successfully"
+    })
+};
+
+
+
---
+++ server/server.js
@@ -0,0 +1,18 @@
+import  express from "express";
+import dotenv from "dotenv";
+import routes from "./routes/routes.js"
+import connectDB from "../server/config/db.js"
+
+dotenv.config();
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use("/api/notes", routes);
+connectDB();
+
+
+
+app.listen(5000, () => {
+    console.log(`server started on http://localhost:${port}`)
+})
