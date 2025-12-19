6f80c588c46ba702bfafe20c52493d89b4cdd83f
kazedevs <businessrajkumardas@gmail.com>
2025-12-19 13:28:29 +0530

    changed prouction url

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://think-board-one.vercel.app"],
     credentials: true
 }));
 app.use(rateLimiter);
